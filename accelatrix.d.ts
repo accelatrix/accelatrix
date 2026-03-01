9e833e0cae6e35c560df11bdfd0192fb0a1ddf87
C Miguel Ferreira <Miguel@Ferreira-family.org>
2026-03-01 21:25:37 +0100

    v1.7.5

--- accelatrix.d.ts
+++ accelatrix.d.ts
@@ -28,7 +28,7 @@
     }
 /** Accelatrix namespace. */
 declare namespace Accelatrix {
-    const Version = "1.7.4";
+    const Version = "1.7.5";
     /** A base exception. */
     class Exception extends Error {
         constructor(message: string);
--- accelatrix.module.d.ts
+++ accelatrix.module.d.ts
@@ -30,7 +30,7 @@ declare global {
 }
 /** Accelatrix namespace. */
 export declare namespace Accelatrix {
-    const Version = "1.7.4";
+    const Version = "1.7.5";
     /** A base exception. */
     class Exception extends Error {
         constructor(message: string);
